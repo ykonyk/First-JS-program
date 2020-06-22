43626f9f810cd01461e045304997ebbe7e000989
ykonyk <66778509+ykonyk@users.noreply.github.com>
2020-06-22 11:23:26 +0200

    update text

--- code.js
+++ code.js
@@ -1,1 +1,24 @@
-
+switch (education){
+     case "no high school diploma":
+         salary += "$25,636";
+         break;
+     case "a high school diploma":
+         salary += "$35,256";
+         break;
+     case "an Associate's degree":
+         salary += "$41,496";
+         break;
+     case "a Bachelor's degree":
+         salary += "$59,124";
+         break;
+     case "a Master's degree":
+         salary += "$69,732";
+         break;
+     case "a Professional degree":
+         salary += "$89,960";
+         break;
+     case "a Doctoral degree":
+         salary += "$84,396";
+    }
+ 
+ console.log("In 2015, a person with "+education+" earned an average of "+salary+"/year.")
